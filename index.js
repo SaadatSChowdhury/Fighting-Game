e3b2adbcc3a0d3d835831a562b5b3ec103ae0f2d
Saadat Sarwar Chowdhury <saadatc2002@gmail.com>
2024-01-29 16:29:10 -0500

    Finish

--- index.js
+++ index.js
@@ -177,6 +177,8 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
     background.update()
     shop.update()
+    c.fillStyle = 'rgba(255, 255, 255, 0.15)'
+    c.fillRect(0,0, canvas.width,canvas.height)
     player.update()
     enemy.update()
 
@@ -231,7 +233,9 @@ function animate() {
     {
         enemy.takeHit()
         player.isAttacking = false
-        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+        gsap.to('#enemyHealth',{
+            width: enemy.health + '%'
+        })
     }
 
     // if player misses
@@ -249,7 +253,9 @@ function animate() {
     {
         player.takeHit()
         enemy.isAttacking = false
-        document.querySelector('#playerHealth').style.width = player.health + '%'
+        gsap.to('#playerHealth',{
+            width: player.health + '%'
+        })
     }
 
     if(enemy.isAttacking && enemy.framesCurrent === 2){
